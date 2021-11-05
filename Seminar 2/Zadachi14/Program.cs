c193d91884aa2a0760649827e5d43f61d0e56705
Salabon <sergeistarshark@mail.ru>
2021-11-05 16:42:47 +0300

    сделал 4 задачи

--- Seminar 2/Zadachi14/Program.cs
+++ Seminar 2/Zadachi14/Program.cs
@@ -1,1 +1,63 @@
-﻿
+﻿        // ========================== Задача 1===========================
+        // int Method1(int a)
+        // {
+        //     int proizv = a * a;
+        //     return proizv;
+        // }
+        // int A = Method1(5);
+        // Console.WriteLine($"Квадрат введенного числа равен: {A}");
+
+        // ========================== Задача 2===========================
+        // int a = 4;
+        // int b = 3;
+        // int res = b * b;
+        // Console.WriteLine();
+        // Console.WriteLine($"первое число равно {a}");
+        // Console.WriteLine();
+        // Console.WriteLine($"второе число равно {b}");
+        // Console.WriteLine();
+        // Console.WriteLine($"квадрат второго числа равен {res}");
+        // Console.WriteLine();
+        // if ( b * b == a) 
+        //     {
+        //         Console.WriteLine("Да, действительно. Первое число является квадратом второго");
+        //     }
+        //     else 
+        //     {
+        //         Console.WriteLine("Нет первое число не является квадратом второго");
+        //     }
+        //     Console.WriteLine();
+
+
+        // ========================== Задача 3===========================
+        // int a = 44;
+        // int b = 122;
+
+        // Console.WriteLine();
+        // Console.WriteLine($"первое число равно {a}");
+        // Console.WriteLine();
+        // Console.WriteLine($"второе число равно {b}");
+        // Console.WriteLine();
+        // if ( a > b ) 
+        //     {
+        //         Console.WriteLine($"Первое число больше, так как {a} больше, чем {b}");
+        //     }
+        //     else 
+        //     {
+        //         Console.WriteLine($"Второе число больше, так как {b} больше, чем {a}");
+        //     }
+        //     Console.WriteLine();
+    
+    
+        // ========================== Задача 4===========================
+
+
+        // int a = 7;
+
+        // if( a == 1) Console.WriteLine("это понедельник");
+        // if( a == 2) Console.WriteLine("это вторник");
+        // if( a == 3) Console.WriteLine("это среда");
+        // if( a == 4) Console.WriteLine("это четверг");
+        // if( a == 5) Console.WriteLine("это пятница");
+        // if( a == 6) Console.WriteLine("это суббота");
+        // if( a == 7) Console.WriteLine("это воскресенье");
