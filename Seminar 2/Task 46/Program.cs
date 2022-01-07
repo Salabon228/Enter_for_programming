2a3b5921b2c2f7d548ef9928fee763ba72995a67
Salabon <sergeistarshark@mail.ru>
2022-01-07 22:49:23 +0300

    работа над 46

---
+++ Seminar 2/Task 46/Program.cs
@@ -0,0 +1,28 @@
+﻿int[,] GetRandomArray(int rows, int columns)
+{
+    Random random = new Random();
+    var result = new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i,j] = random.Next(100);
+        }   
+    }
+    return result;
+}
+void Main()
+{
+    int rows = 10;
+    int columns = 5;
+    var myArray = GetRandomArray(rows, columns);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            System.Console.Write($"{myArray[i,j]}\t");
+        }
+        System.Console.WriteLine(); 
+    }
+}
+Main();
