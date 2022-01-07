73e9bb8e88260bfd4d3c86d0985dd18be0645de1
Salabon <sergeistarshark@mail.ru>
2022-01-07 13:49:23 +0300

    43 complete

--- Seminar 2/Task 43/Program.cs
+++ Seminar 2/Task 43/Program.cs
@@ -1,1 +1,9 @@
 ﻿// 43. Написать программу преобразования десятичного числа в двоичное
+
+string Transformation(int value)
+{
+    string binary = Convert.ToString(value, 2);
+    return binary;
+}
+
+System.Console.WriteLine(Transformation(22127));
