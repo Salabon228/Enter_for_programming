899a9fe707052486c26d318a20c6213f544bef14
Salabon <sergeistarshark@mail.ru>
2022-01-07 15:02:01 +0300

    add 41 42 43 44 Please enter the commit message for your changes. Lines starting

---
+++ Seminar 2/Task 44/Program.cs
@@ -0,0 +1,10 @@
+﻿// 44. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+int Crossing(int k1, int k2, int b1, int b2)
+{
+    int x, y;
+    x = (b2 - b1)/(k1-k2);
+    y = k1*x + b1;
+    System.Console.Write($"Эти 2 прямые пересекаются в точке {x}, "); 
+    return y;
+}
+System.Console.WriteLine(Crossing(1,2,3,4));
