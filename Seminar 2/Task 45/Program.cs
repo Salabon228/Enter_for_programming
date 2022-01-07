c65ec66d750c7ac6c1caa0681b18f131baede29f
Salabon <sergeistarshark@mail.ru>
2022-01-07 15:10:11 +0300

    add 45

---
+++ Seminar 2/Task 45/Program.cs
@@ -0,0 +1,14 @@
+﻿// 44. Показать числа Фибоначчи
+void Fibonacci()
+{
+    int first = 0;
+    int second = 1;
+    for (int i = 0; i < 45; i++)
+    {
+        int result = first + second;
+        first = second;
+        second = result;
+        System.Console.WriteLine(result);
+    }
+}
+Fibonacci();
