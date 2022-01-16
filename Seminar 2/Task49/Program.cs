6b19adecf61fed9a6a4005c677621c35f9a41178
Salabon <sergeistarshark@mail.ru>
2022-01-16 15:54:46 +0300

    add 49

---
+++ Seminar 2/Task49/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] GetRandomArray(int rows, int columns)
+{
+    Random random = new Random();
+    var result = new double[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {                                                            
+        for (int j = 0; j < columns; j++)
+        {
+            result[i,j] = random.NextDouble();
+        }   
+    }
+    return result;
+}
+int rows = 3;
+int columns = 3;
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            System.Console.Write($"{array[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+PrintArray(GetRandomArray(3,3));
